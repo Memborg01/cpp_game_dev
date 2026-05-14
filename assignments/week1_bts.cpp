30e45b5285797fb85f30c280a7b8ca2d68e45565
Memborg01 <jakmemborg@gmail.com>
2026-05-15 00:30:54 +0200

    Added assignment for week 1

--- assignments/week1_bts.cpp
+++ assignments/week1_bts.cpp
@@ -1,43 +1,87 @@
 #include <iostream>
-#include <string>
 
 struct Node {
+
   int val{0};
   Node *left;
   Node *right;
 };
 
-void PostOrder(Node *subtree) {
+/**
+ * Looks through Binary Tree Search nodes in Post Order
+ * (See reference to Postorder Traversal at:
+ * https://www.geeksforgeeks.org/dsa/tree-traversals-inorder-preorder-and-postorder/)
+ * If leaf node, print value.
+ *
+ * @param *subtree Pointer of the subtree node
+ */
+void PostOrderPrintLeaf(Node *subtree) {
+  if (subtree == nullptr)
+    return;
+
   if (subtree->left == nullptr && subtree->right == nullptr) {
     std::cout << subtree->val << "\n";
     return;
   }
 
-  PostOrder(subtree->left);
-  PostOrder(subtree->right);
+  PostOrderPrintLeaf(subtree->left);
+  PostOrderPrintLeaf(subtree->right);
+}
+
+/**
+ * Places node value at empty place in node tree
+ *
+ * @Param *subtree Reference to the subtree we are in.
+ * @Param *currentNode Reference to the current node we want to place in Binary
+ * Tree
+ * */
+void PlaceValue(Node *subtree, Node *currentNode) {
+  if (currentNode->val < subtree->val) {
+    if (subtree->left == nullptr) {
+      subtree->left = currentNode;
+      return;
+    }
+
+    PlaceValue(subtree->left, currentNode);
+    return;
+  }
+
+  if (subtree->right == nullptr) {
+    subtree->right = currentNode;
+    return;
+  }
+
+  PlaceValue(subtree->right, currentNode);
+  return;
+}
+
+Node CreateBinarySearchTree(Node node_list[9]) {
+  Node root = node_list[0];
+
+  for (int i = 1; i < 9; i++) {
+    if (node_list[i].val == 0) {
+      return root;
+    }
+
+    PlaceValue(&root, &node_list[i]);
+  }
+  return root;
+}
+
+Node BTSNode(int val) {
+  Node node = Node();
+  node.val = val;
+  return node;
 }
 
 int main() {
+  Node num_list[9] = {BTSNode(8),  BTSNode(3),  BTSNode(12),
+                      BTSNode(6),  BTSNode(1),  BTSNode(4),
+                      BTSNode(10), BTSNode(14), BTSNode(9)};
 
-  Node r1_l1 = Node();
-  r1_l1.val = 4;
+  Node newRoot = CreateBinarySearchTree(num_list);
 
-  Node left1_l1 = Node();
-  left1_l1.val = 1;
-  Node left1_r1 = Node();
-  left1_r1.val = 6;
-  left1_r1.left = &r1_l1;
-
-  Node left1 = Node();
-  left1.val = 3;
-  left1.left = &left1_l1;
-  left1.right = &left1_r1;
-
-  Node root = Node();
-  root.val = 8;
-  root.left = &left1;
-
-  PostOrder(&root);
+  PostOrderPrintLeaf(&newRoot);
 
   return 0;
 }
