16a2279232a2ae80da124ff0fa56eb3a2048bbc6
Memborg01 <jakmemborg@gmail.com>
2026-05-16 14:46:41 +0200

    Added - Lecture files on namespaces

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -15,6 +15,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - - two_sum.cpp
 - - oop_practice program
 - - main_app.cpp
+- - Lecture files on namespaces
 
 ## [0.0.4] - 2026-05-15
 
---
+++ samples/namespaces/namespaces.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+
+namespace main_character {
+void walk() { std::cout << "The main character is walking\n"; }
+} // namespace main_character
+
+namespace enemy {
+void walk() { std::cout << "The enemy is walking\n"; }
+} // namespace enemy
+
+namespace npc {
+void walk() { std::cout << "The npc is walking\n"; }
+} // namespace npc
